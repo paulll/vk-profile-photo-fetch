e474ca18d8f389c9d8ed0e1969ad9aef81ffe66c
paulll <paulll@derpy.ru>
2019-02-19 01:00:13 +0300

    disable proxy by default

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 module.exports = {
 	redis_url: 'redis://localhost',
 	local_csv: 'dumped.csv',
-	users_per_request: 26,
+	users_per_request: 25,
 	request_interval: 1000/3,
 	tokens: [
 		'there was a private token',
@@ -10,7 +10,7 @@ module.exports = {
 		'there was a private token',
 		'there was a private token'
 	],
-	socks: Array(10) // количество
+	socks: Array(0) // количество
 		.fill(10000) // стартовый порт
 		.map((x,i)=>`http://user:password@proxy.local:${x+i}`)
 	// можно указать и просто списком ['http://...', 'http://...']
