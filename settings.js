b54068ede6ef7e00601c6513f6c2fdb35f3b0cad
paulll <paulll@derpy.ru>
2019-02-19 09:46:38 +0300

    change default users_per_request to 25

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ module.exports = {
 	redis_url: 'redis://localhost',
 	local_csv: 'dumped.csv',
 	redis_key: 'vpd:current_user',
-	users_per_request: 15,
+	users_per_request: 25,
 	request_interval: 1000/3,
 	tokens: [
 		'there was a private token',
